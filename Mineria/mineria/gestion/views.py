df48e852dfc75e62abce4c16834647bc3b5d177c
jhoan28310576 <jhoan19634@gmail.com>
2024-02-09 02:21:12 -0400

    gestion
    se realiso el diceno de gestion  falta obntener los datos indibidualmete

--- Mineria/mineria/gestion/views.py
+++ Mineria/mineria/gestion/views.py
@@ -7,4 +7,10 @@ def index(request):
     gestions =  gestion.objects.all()
     return render (request, "index.html",{"gestion":gestions})
     
+def individual(request):
+    try:
+        contenido = gestion.objects.get(id=1)  # Cambia 'MiModelo' por el nombre de tu modelo y '1' por el ID del objeto que quieres obtener
+    except gestion.DoesNotExist:
+        raise Http404("El contenido no existe")
+    return render(request, 'index.html', {'individuall': contenido})
     
