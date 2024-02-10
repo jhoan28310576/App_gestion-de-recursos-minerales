f4d8d0237aeba1b88f66a4f566a32c56cd3a7c70
jhoan28310576 <jhoan19634@gmail.com>
2024-02-10 02:12:48 -0400

    gestion
    se realizo las modifiaciones de la vista agregando los ide de la informacion que va en gestion  y renderizandolos ene l html de gestion

--- Mineria/mineria/gestion/views.py
+++ Mineria/mineria/gestion/views.py
@@ -4,13 +4,13 @@ from .model import gestion
 
 # Create your views here.
 def index(request):
-    gestions =  gestion.objects.all()
-    return render (request, "index.html",{"gestion":gestions})
-    
-def individual(request):
     try:
-        contenido = gestion.objects.get(id=1)  # Cambia 'MiModelo' por el nombre de tu modelo y '1' por el ID del objeto que quieres obtener
+        gestions = gestion.objects.filter(id__in=[1, 2, 3])
     except gestion.DoesNotExist:
         raise Http404("El contenido no existe")
-    return render(request, 'index.html', {'individuall': contenido})
+    return render(request, 'index.html', {'gestion': gestions})
+
+
+
+
     
