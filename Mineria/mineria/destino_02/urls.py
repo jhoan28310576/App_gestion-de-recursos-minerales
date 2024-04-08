9116f18a462be6082b9c0656e0032535ea2857b3
jhoan28310576 <jhoan19634@gmail.com>
2024-04-08 17:45:34 -0400

    v15
    se finalizo las funciones de los botones de ver informacion  y de eliminar. en el app de destino_02

--- Mineria/mineria/destino_02/urls.py
+++ Mineria/mineria/destino_02/urls.py
@@ -1,9 +1,13 @@
 from django.urls import path
 from . import views
+from .views import DeletedestinoView
+
 
 urlpatterns = [
     path('', views.destino, name='destino'),
-    path('listar', views.destino_list_view, name='destino_list_view')
+    path('listar', views.destino_list_view, name='destino_list_view'),
+    path('load_destino_details/', views.load_destino_details, name='load_destino_details'), 
+    path('delete_destino/<int:id>/', DeletedestinoView.as_view(), name='delete_destino'),
 ]
 
 
--- Mineria/mineria/destino_02/views.py
+++ Mineria/mineria/destino_02/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from .forms import Destinoform
 from .models import Destino
+from django.http import JsonResponse
+from django.views import View
 from  django.contrib import messages
 # Create your views here
 def destino(request):
@@ -16,6 +18,17 @@ def destino(request):
 
 def destino_list_view(request):
     destino = Destino.objects.all()
-    return render(request, 'listar_destinos.html', {'destinos': destino})
+    return render(request, 'listar_destinos.html', {'destino': destino})
 
 
+def load_destino_details(request):
+    destino_id = request.GET.get('id')
+    destino = Destino.objects.get(id=destino_id)
+    data = {'estado': destino.estado, 'ciudad': destino.ciudad, 'direccion': destino.direccion}
+    return JsonResponse(data)
+
+class DeletedestinoView(View):
+    def  delete(self, request, *args, **kwargs):
+        destino = Destino.objects.get(pk=self.kwargs['id'])
+        destino.delete()
+        return JsonResponse({'success': True})
