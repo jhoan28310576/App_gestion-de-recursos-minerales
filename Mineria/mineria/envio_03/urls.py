15457b4c5eb48f1243757d1538784f4f470be7a9
jhoan28310576 <jhoan19634@gmail.com>
2024-04-08 22:10:42 -0400

    v15
     se creo las funciones de eliminar  de destino y  se creo la pagina de envio mas la vista y funcion de la lista de envios y el grafico  y la funciones de ver informacion, falto de eliminar y colocar  lo botones de redirecciona

--- Mineria/mineria/envio_03/urls.py
+++ Mineria/mineria/envio_03/urls.py
@@ -3,4 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.envio, name='envio'),
+    path('listar/', views.envio_list_view, name='envio_list_view'),
+    path('load_envio_details/', views.load_envio_details, name='load_envio_details'),
+    
 ]
--- Mineria/mineria/envio_03/views.py
+++ Mineria/mineria/envio_03/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
 from .forms import Envioform
+from .models import Envio
+from django.http import JsonResponse
+from django.core.serializers import serialize
+from django.core.serializers.json import DjangoJSONEncoder
 from  django.contrib import messages
 # Create your views here
 def envio(request):
@@ -12,3 +16,27 @@ def envio(request):
     else:
         envio = Envioform()
     return render(request,'envio.html',{"envios":envio})
+
+def envio_list_view(request):
+    envio = Envio.objects.all()
+    return render(request, 'listar_envios.html',{'envio': envio})
+
+class MineralSerializer(DjangoJSONEncoder):
+    def default(self, obj):
+        if isinstance(obj, models.Model):
+            return obj.nombre  # Suponiendo que 'nombre' es un campo serializable de Mineral
+        return super().default(obj)
+    
+def load_envio_details(request):
+    envio_id = request.GET.get('id')
+    envio = Envio.objects.get(id=envio_id)
+    
+    data = {
+        'mineral': envio.mineral.nombre,  # Suponiendo que 'nombre' es un campo serializable de Mineral
+        'transporte': envio.transporte.matricula,
+        'destino': envio.destino.estado,
+        'fecha_envio': envio.fecha_envio.strftime('%Y-%m-%d %H:%M:%S'),
+        'cantidad_mineral': str(envio.cantidad_mineral)
+    }
+    
+    return JsonResponse(data)
